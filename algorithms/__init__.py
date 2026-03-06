a32d53178fcce9dc6a2a4ee19c32ac3fe673124c
KhanhTuan-Hash <khanhtuantk04@gmail.com>
2026-03-06 21:50:23 +0000

    Update

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,19 @@
-from .evolutionary.genetic_algorithm import GeneticAlgorithm
+from .classic.hill_climbing import HillClimbing
+from .classic.tabu_search import TabuSearch
+from .physic_based.simulated_annealing import SimulatedAnnealing
+from .evolutionary.genetic_algorithm import GeneticAlgorithm, GeneticAlgorithm_Discrete, GeneticAlgorithm_Continuous
 from .evolutionary.differential_evolution import DifferentialEvolution
-from .evolutionary.ga import GA
-from .evolutionary import ga
-__all__ = ['GeneticAlgorithm', 'GA', 'DifferentialEvolution', 'ga']
+__all__ = [
+    'LocalSearch',
+    'TabuSearch',
+    'SimulatedAnnealing',
+    'GeneticAlgorithm',
+    'GeneticAlgorithm_Discrete',
+    'GeneticAlgorithm_Continuous',
+    'DifferentialEvolution'
+]
+#from .evolutionary.genetic_algorithm import GeneticAlgorithm
+#from .evolutionary.differential_evolution import DifferentialEvolution
+#from .evolutionary.ga import GA
+#from .evolutionary import ga
+#__all__ = ['GeneticAlgorithm', 'GA', 'DifferentialEvolution', 'ga']
